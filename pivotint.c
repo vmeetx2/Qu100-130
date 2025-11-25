1ea43ade853332eabe889bf9697068e99d1d4389
Veer Meet <vmeet.research@gmail.com>
2025-11-26 00:38:39 +0530

    Update pivotint.c

--- pivotint.c
+++ pivotint.c
@@ -5,4 +5,4 @@ int main(){
     printf("-1\n");
 }
 
-//Write a Program to take an array of integers as input, calculate the pivot index of this array. The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right. If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array. Print the leftmost pivot index. If no such index exists, print -1.
+//Write a Program to take a positive integer n as input, and find the pivot integer x such that the sum of all elements between 1 and x inclusively equals the sum of all elements between x and n inclusively. Print the pivot integer x. If no such integer exists, print -1. Assume that it is guaranteed that there will be at most one pivot integer for the given input.
